192cd19c7a37984647dbb74f3d1573df9414bd71
Marek <korzeniowskimarek.mk@gmail.com>
2025-10-08 11:18:21 +0200

    update.

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -105,6 +105,90 @@ def test_remove_summary_incorrect_id(test_app_with_db):
     }
 
 
+@pytest.mark.parametrize(
+    "summary_id, payload, status_code, detail",
+    [
+        [
+            999,
+            {"url": "https://foo.bar/", "summary": "updated!"},
+            404,
+            "Summary not found",
+        ],
+        [
+            0,
+            {"url": "https://foo.bar/", "summary": "updated!"},
+            422,
+            [
+                {
+                    "type": "greater_than",
+                    "loc": ["path", "id"],
+                    "msg": "Input should be greater than 0",
+                    "input": "0",
+                    "ctx": {"gt": 0},
+
+                }
+            ],
+        ],
+        [
+            1,
+            {},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "url"],
+                    "msg": "Field required",
+                    "input": {},
+
+                },
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {},
+
+                },
+            ],
+        ],
+        [
+            1,
+            {"url": "https://foo.bar/"},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {"url": "https://foo.bar/"},
+
+                }
+            ],
+        ],
+    ],
+)
+def test_update_summary_invalid(
+    test_app_with_db, summary_id, payload, status_code, detail
+):
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/", data=json.dumps(payload)
+    )
+    assert response.status_code == status_code
+    print(response.json()["detail"])
+    assert response.json()["detail"] == detail
+
+
+def test_update_summary_invalid_url(test_app):
+    response = test_app.put(
+        "/summaries/1/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
+    )
+    assert response.status_code == 422
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
+
+
+
 def test_update_summary(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar/"})
